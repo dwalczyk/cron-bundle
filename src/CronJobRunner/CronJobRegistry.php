b3620e1a9c4ff13e0eb4d70a5acd9c5a2f5de422
Dawid Walczyk <bodacz6512@gmail.com>
2022-12-20 22:58:34 +0100

    x

--- src/CronJobRunner/CronJobRegistry.php
+++ src/CronJobRunner/CronJobRegistry.php
@@ -6,14 +6,25 @@ use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 
 final class CronJobRegistry implements CronJobRegistryInterface
 {
+    private readonly array $jobs;
+
     public function __construct(
         #[TaggedIterator('cron.job')] iterable $jobs
     )
     {
+        $arrayJobs = [];
+
+        foreach ($jobs as $job) {
+            $arrayJobs[] = $job;
+        }
+
+        $this->jobs = $arrayJobs;
     }
 
     public function all(): iterable
     {
-        // TODO: Implement all() method.
+        print_r($this->jobs);
+
+        return [];
     }
 }
