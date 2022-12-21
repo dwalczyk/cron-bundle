4fe59ec3a20a832c59dac38520b6a98eadd5d244
Dawid Walczyk <bodacz6512@gmail.com>
2022-12-21 10:02:43 +0100

    x

---
+++ src/DependencyInjection/CronExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dawid\CronBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class CronExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $this->loadServices($container);
+    }
+
+    private function loadServices(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
